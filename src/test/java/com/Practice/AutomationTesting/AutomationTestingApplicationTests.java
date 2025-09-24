b6c44e921197de374c1215c7c03af449a7302076
PranayShivam <pronoybose7@gmail.com>
2025-09-25 00:43:44 +0530

    First Push

---
+++ src/test/java/com/Practice/AutomationTesting/AutomationTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practice.AutomationTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
